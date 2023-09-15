65cedf1ecc1f0dd3f2a3804d0e648ee1224eddee
VladimirSemen2022 <107301979+VladimirSemen2022@users.noreply.github.com>
2023-09-15 13:00:57 +0300

    Добавьте файлы проекта.

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using My_food_manager.ViewModel;
+using System.Windows;
+
+namespace My_food_manager
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainVM();
+        }
+    }
+}
